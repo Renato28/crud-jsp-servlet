10d83c7365b7c4d63772f90d3d90e52623e00351
Renato Nobrega <renatonobrega87@gmail.com>
2026-05-30 13:47:43 -0300

    implementando o metodo de listar usuarios

--- src/main/java/br/com/github/renato28/projetocrudjspservlet/dao/UsuarioDAO.java
+++ src/main/java/br/com/github/renato28/projetocrudjspservlet/dao/UsuarioDAO.java
@@ -1,10 +1,13 @@
 package br.com.github.renato28.projetocrudjspservlet.dao;
 
-import br.com.github.renato28.projetocrudjspservlet.ConnectionFactory;
+import br.com.github.renato28.projetocrudjspservlet.connection.ConnectionFactory;
 import br.com.github.renato28.projetocrudjspservlet.model.Usuario;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UsuarioDAO {
 
@@ -12,15 +15,47 @@ public class UsuarioDAO {
 
         String sql = "INSERT INTO usuario (nome,email) VALUES (?,?,?)";
 
-        try(Connection conn = ConnectionFactory.getConnection()){
+        try (Connection conn = ConnectionFactory.getConnection()) {
 
-            PreparedStatement  stmt = conn.prepareStatement(sql);
+            PreparedStatement stmt = conn.prepareStatement(sql);
 
             stmt.setString(1, usuario.getNome());
             stmt.setString(2, usuario.getEmail());
 
+            stmt.executeUpdate();
+
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public List<Usuario> listar() {
+
+        List<Usuario> usuarios = new ArrayList<>();
+
+        String sql = "SELECT * FROM usuario";
+
+        try (Connection connection = ConnectionFactory.getConnection();
+
+             PreparedStatement statement = connection.prepareStatement(sql);
+
+             ResultSet resultSet = statement.executeQuery()) {
+
+            while (resultSet.next()) {
+
+                Usuario usuario = new Usuario();
+
+                usuario.setId(resultSet.getLong("id"));
+
+                usuario.setNome(resultSet.getString("nome"));
+
+                usuario.setEmail(resultSet.getString("email"));
+
+                usuarios.add(usuario);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return usuarios;
     }
 }
